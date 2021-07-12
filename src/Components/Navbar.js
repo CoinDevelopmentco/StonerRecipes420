bf8ed71f2059651eab626e8d2bab5fc2aa72d273
CoinDevelopmentco <CoinDevelopmentco@gmail.com>
2021-07-12 12:19:15 -0700

    addedPropa

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,21 +4,15 @@ import './Navbar.css';
 function Navbar() {
     return(
         <div className="Navbar">
-            <td className="c1">
-                <tr>
-                    <h1>StonerRecipes.net</h1>
-                </tr>
-            </td>
-            <td className="c2">
-                <tr>
-                    <h2>WILL_EDIT_LATER</h2>
-                </tr>
-            </td>
-            <td className="c3">
-                <tr>
+            <div className="c1">
+                    <h1>LOGO</h1>
+            </div>
+            <div className="c2">
+                    <h1>StonerRecipes</h1>
+            </div>
+            <div className="c3">
                     <h3>(legalize-it)</h3>
-                </tr>
-            </td>
+            </div>
         </div>
     );
 }
