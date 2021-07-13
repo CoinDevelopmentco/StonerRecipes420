bc0110cdbb43474dcfa01120a1142cdf7047448c
CoinDevelopmentco <CoinDevelopmentco@gmail.com>
2021-07-12 20:09:02 -0700

    editMoreText

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -8,10 +8,10 @@ function Instructions(props) {
             <div className="title">Instructions</div>
             <div className="instructionSpan" />
             <div>Pre-heat oven to 220f</div>
-            <div>Place finely ground <a className="link" href="https://www.google.com/search?q=define%3Acannabis&source=hp&ei=PMfsYJyVPM_1-gShyZ-4CA&oq=define%3Acannabis&gs_lcp=ChFtb2JpbGUtZ3dzLXdpei1ocBADOgIIADoLCC4QsQMQxwEQowI6CAguEMcBEKMCOggILhCxAxCDAToFCAAQsQM6DgguELEDEMcBEKMCEJMCOggIABCxAxCDAToFCC4QsQNQ-y5YwVlg9FpoAnAAeAGAAcYCiAGeE5IBCDEwLjIuMy4ymAEAoAEBsAEA&sclient=mobile-gws-wiz-hp" >Cannabis</a> onto a pacrchment lined baking sheet.</div>
+            <div>Place finely ground <a className="link" href="https://www.google.com/search?q=define%3Acannabis&source=hp&ei=PMfsYJyVPM_1-gShyZ-4CA&oq=define%3Acannabis&gs_lcp=ChFtb2JpbGUtZ3dzLXdpei1ocBADOgIIADoLCC4QsQMQxwEQowI6CAguEMcBEKMCOggILhCxAxCDAToFCAAQsQM6DgguELEDEMcBEKMCEJMCOggIABCxAxCDAToFCC4QsQNQ-y5YwVlg9FpoAnAAeAGAAcYCiAGeE5IBCDEwLjIuMy4ymAEAoAEBsAEA&sclient=mobile-gws-wiz-hp" >Cannabis</a> onto a pacrchment lined baking sheet and then into the oven</div>
             <div>Let the marijuana <a className="link" href="https://www.google.com/search?q=define%3ADecarboxylate&bih=654&biw=1440&hl=en&ei=jsfsYLLpItWE-gTWqIzgBA&oq=define%3ADecarboxylate&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsANKBAhBGABQ6hhY6hhgsyBoAXACeACAAUyIAY0BkgEBMpgBAKABAaoBB2d3cy13aXrIAQjAAQE&sclient=gws-wiz&ved=0ahUKEwiy6Py_z97xAhVVgp4KHVYUA0wQ4dUDCA4&uact=5">Decarboxylate</a> for 1 hour.</div>
             <div className="instructionSpan" />
-            <div>Find the largest pot available to fill with water, butter and REEFER.</div>
+            <div>Fill the largest pot available with water, butter and REEFER.</div>
             <div>As the Ganja finishes to decarboxylate bring the water/butter solution to a slow simmer.</div>
             <div>Place the fully decarboxylated herb into the lightly simmering solution for 1-4hrs.</div>
             <div className="emphasize">Stir and SMOKE WE3D as needed.</div>
@@ -20,7 +20,7 @@ function Instructions(props) {
             <div>Separate the cannabis from butter/h20.</div>
             <div>Refrigerate <a className="link" href="https://www.google.com/search?q=define%3Acannabutter&ei=kP3sYMiPDomU-gSY_LTQDg&oq=define%3Acannabutter&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAELEDEA06BAgAEA06BggAEAcQHjoICAAQCBAHEB46BQgAEIYDSgQIQRgAUKgnWPMuYOsxaABwAHgAgAGtAYgBnwaSAQM2LjKYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=gws-wiz&ved=0ahUKEwjIl8iAg9_xAhUJip4KHRg-DeoQ4dUDCA4&uact=5" >Cannabutter</a> mixture for 24-48 hrs.</div>
             <div className="emphasize">Clean up</div>
-            <div>Drain and separate the water away from butter to have your final form(Cannabutter).</div>
+            <div>Drain and separate the water away from butter to have your final product(Cannabutter).</div>
             <div>At this point; you can either use the Cannabutter, or clean it up.</div>
             <div className="instructionSpan" />
             <div className="emphasize">To clean the butter for better taste:</div>
